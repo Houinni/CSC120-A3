b7ddbf0b8f17254b1cb0b6d257b1d53115e6a3f6
Jinki Hou <jhou@smith.edu>
2024-09-23 18:31:46 -0400

    print a transcript

--- Conversation.java
+++ Conversation.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Conversation {
@@ -8,22 +10,34 @@ class Conversation {
     Scanner input = new Scanner(System.in);
     //create a scanner object to read input from console
 
+    List<String> transcript = new ArrayList<>();
+    //create a list for transcript
+
     int round=0;
     int cnt=0;
-    System.out.println("please type in how many round");
+    System.out.print("How many round? ");
     round = input.nextInt();
     System.out.println("great let's start talking "+ round + " round, what do you want to talk about?");
     //set the round limit
-    input.nextLine();
-    
-
+    String talk = input.nextLine();
+    //
     
     while(cnt<round){
-      System.out.println("you: ");
-      String talk=input.nextLine();
-      System.out.println("bot: humm");
+      System.out.print("you: ");
+      talk=input.nextLine();
+      String botReply="humm";
+      System.out.println(botReply);
       cnt++;
+      transcript.add(talk);
+      transcript.add(botReply);
     }
+    //do the talking
+
+    System.out.println("\n--- Conversation Transcript ---");
+    for (String line : transcript) {
+      System.out.println(line);
+    }
+    //print a transcript
 
   }
 }
